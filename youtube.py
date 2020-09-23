17f04b038d3007229a9a6581a3d441b2910de079
Kaiose31 <saurabhpahwa31@gmail.com>
2020-09-23 07:11:59 +0530

    bla

--- youtube.py
+++ youtube.py
@@ -3,12 +3,22 @@ import urllib.request,urllib.parse,json
 import time
 from selenium import webdriver
 
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient
+
+#get the ids of all subscribed channels
+
+def get_id():
+    pass
 
 
 def look_for_new_video():
-    api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    
+    f = open('api_key.txt')
+    api_key = f.readline()
     channel_id = "AIzaSyAG4S9ymL9jxZt_tThc5FCB7rV3E_ebAEE"
-
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
